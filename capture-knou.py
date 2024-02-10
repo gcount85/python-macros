c9c84a9f563e68dc4da6701db33b55f3609adf60
gcount85 <100mgml@gmail.com>
2024-02-10 16:32:25 +0900

    변수

--- capture-knou.py
+++ capture-knou.py
@@ -3,10 +3,11 @@ import time
 
 
 pyautogui.FAILSAFE = True
+screen_shot_key = 'f12'
 
 
 def press_printscreen():
-    pyautogui.press('f12')
+    pyautogui.press(screen_shot_key)
     # with pyautogui.hold(['shiftleft, command']):
     #     pyautogui.press(['5'])
 
@@ -25,66 +26,25 @@ def go_next_page():
     pyautogui.press('right')
 
 
-def close_window():
-    pyautogui.click(x=-260, y=694, interval=1) 
-    pyautogui.press(['command, w'])
-    time.sleep(0.5)
-    pyautogui.press(['tab, return'])
+# def close_window():
+#     pyautogui.click(x=-260, y=694, interval=1) 
+#     pyautogui.press(['command, w'])
+#     time.sleep(0.5)
+#     pyautogui.press(['tab, return'])
 
 
-def open_viewer():
-    pyautogui.click(x=-1167, y=603, interval=1) 
-    pyautogui.click(x=-873, y=696, interval=1) 
-    time.sleep(0.5)
-    pyautogui.press(['command, w'])
-    time.sleep(0.5)
-    pyautogui.click(x=-872, y=153, interval=1) 
+# def open_viewer():
+#     pyautogui.click(x=-1167, y=603, interval=1) 
+#     pyautogui.click(x=-873, y=696, interval=1) 
+#     time.sleep(0.5)
+#     pyautogui.press(['command, w'])
+#     time.sleep(0.5)
+#     pyautogui.click(x=-872, y=153, interval=1) 
 
 
-def set_viewer():
-    pyautogui.click(x=-260, y=694, interval=1) 
-    pyautogui.click(x=-503, y=72, interval=1)
-
-# def tgle_open_and_check_end():
-#     for _ in range(5):
-#         pgdn_and_toggle()
-#     if (pyautogui.locateOnScreen('end.png', confidence=0.90)):
-#         return
-#     else:
-#         tgle_open_and_check_end()
-
-
-# def copy_to_notion():
-#     time.sleep(3)
-
-#     pyautogui.tripleClick(x=244, y=322)  # 코딩애플 동영상 제목
-#     pyautogui.hotkey('ctrl', 'c', interval=1)
-
-#     # pyautogui.click(x=1192, y=468, interval=1)  # 노션 창으로 이동 및 클릭
-#     pyautogui.click(x=1281, y=555, interval=1)  # 노션 창으로 이동 및 클릭
-#     pyautogui.press(['enter'])
-#     pyautogui.press(['enter'])
-#     time.sleep(1)
-#     pyautogui.write('/')  # 괄호 안의 문자를 타이핑 합니다.
-#     time.sleep(1)
-#     pyautogui.press(['down'])
-#     pyautogui.press(['enter'])
-#     time.sleep(1)
-#     pyautogui.hotkey('ctrl', 'v', interval=1)
-#     pyautogui.press(['enter'])
-
-#     pyautogui.doubleClick(x=244, y=322)  # 크롬으로 이동 및 클릭
-
-#     tgle_open_and_check_end()
-#     pyautogui.hotkey('ctrl', 'a', interval=1)
-#     pyautogui.hotkey('ctrl', 'c', interval=1)
-
-#     pyautogui.click(x=1345, y=18, interval=1)  # 노션 내용 칸으로 이동
-#     time.sleep(1)
-#     pyautogui.hotkey('ctrl', 'v', interval=1)
-#     pyautogui.hotkey('ctrl', '[', interval=1)
-
-#     pyautogui.click("next.png", interval=1)  # 다음 강의 가기
+# def set_viewer():
+#     pyautogui.click(x=-260, y=694, interval=1) 
+#     pyautogui.click(x=-503, y=72, interval=1)
 
 
 num = 41     # 몇 회 반복할 건지 설정
@@ -106,7 +66,6 @@ for f in range(num):
 
 # 현재 마우스 위치 출력
 # print(pyautogui.position())
-# Point(x=882, y=917) 옵션 버튼
 
 
 # pause 문구
